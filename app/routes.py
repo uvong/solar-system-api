65810aeb81014aa02e46ef0aff1537cccb4d58cb
Kelly Kang <shkx281@gmail.com>
2022-04-22 11:12:32 -0700

    Finished first part of Wave 1.

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,13 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id, name, description):
+        self.id = id
+        self.name = name
+        self.description = description
+
+planets = [
+    Planet(1, "Mercury", "named for the messenger of Roman gods"),
+    Planet(2, "Venus", "named for the Roman goddess of love and beauty"),
+    Planet(3, "Neptune", "named for the Roman god of water")
+]
