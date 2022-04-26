076557453ccfbf611199aaabc0cc59894736c963
Kelly Kang <shkx281@gmail.com>
2022-04-26 11:21:56 -0700

    Created a get info method for planet class.

--- app/routes/planets.py
+++ app/routes/planets.py
@@ -6,6 +6,13 @@ class Planet:
         self.name = name
         self.description = description
         self.moons = moons
+    def get_planet_info(self):
+        return {
+                'id': self.id,
+                'name': self.name,
+                'description': self.description,
+                'moons': self.moons
+            }
 
 planets = [
     Planet(1, "Mercury", "named for the messenger of Roman gods", 0),
@@ -18,12 +25,7 @@ planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 def get_all_planets():
     response = []
     for planet in planets:
-        response.append({
-            'id': planet.id,
-            'name': planet.name,
-            'description': planet.description,
-            'moons': planet.moons
-        })
+        response.append(planet.get_planet_info())
     return jsonify(response)
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
@@ -37,13 +39,7 @@ def get_one_planet(planet_id):
     chosen_planet = None
     for planet in planets:
         if planet.id == planet_id:
-            chosen_planet = {
-                'id': planet.id,
-                'name': planet.name,
-                'description': planet.description,
-                'moons': planet.moons
-            }
-
+            chosen_planet = planet.get_planet_info()
     if chosen_planet is None:
         return jsonify({'msg': f"Planet {planet_id} not found."}), 404
     return jsonify(chosen_planet)
